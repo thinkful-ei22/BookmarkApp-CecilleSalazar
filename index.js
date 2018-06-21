3bde6b6763dbfc10f95ea8d161d8f48a1a8d8b4b
Cecille Salazar <cecillersalazar@gmail.com>
2018-06-20 22:36:20 -0700

    Added store with default values

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+'use strict'
+
+const store = {
+  items: [
+    {title: 'Youtube', address: 'https://www.youtube.com/', description: 'My favorite video hosting website.', rating: 5},
+    {title: 'Pandora', address: 'https://www.pandora.com/', description: 'My favorite online radio website.', rating: 5},
+    {title: 'NPR', address: 'https://www.npr.org/', description: 'My favorite news website.', rating: 5},
+  ]
+}
+
+
+
+//DROPDOWN FUNCTION
+
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
@@ -6,10 +20,10 @@ function myFunction() {
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
 
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
+    let dropdowns = document.getElementsByClassName("dropdown-content");
+    let i;
     for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
+      let openDropdown = dropdowns[i];
       if (openDropdown.classList.contains('show')) {
         openDropdown.classList.remove('show');
       }
